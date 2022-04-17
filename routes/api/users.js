5aac57a40eafbd837ff1f464f63a334d70743a05
Hammad <muhammadhammadali112233@gmail.com>
2022-04-17 23:19:26 +0500

    Here we Implement the jwt. first in default.json we create the secret Key . second the payload . third jwt.sign and apply payload , getSecret, and call back function . But token is not get

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,48 +1,50 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
-const router=express.Router();
-const gravatar=require('gravatar');
-const bcrypt=require('bcryptjs');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const config = require('config');
 
-const User=require('../../models/User')
+const User = require('../../models/User')
 
 // @route     POST api/users
 // @desc      Register User
 // @access    Publie     for token 
 
-router.post('/',[
-    check('name','Name is Required')
-    .not()
-    .isEmpty(),
-    check('email','Email is Required').isEmail(),
-    check('password','Please Enter a Password with 6 or More Characters').isLength({min:6}),
+router.post('/', [
+    check('name', 'Name is Required')
+        .not()
+        .isEmpty(),
+    check('email', 'Email is Required').isEmail(),
+    check('password', 'Please Enter a Password with 6 or More Characters').isLength({ min: 6 }),
 
-],async(req,res)=>{
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()})
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
     }
     console.log(req.body)
-    
-    const {name,email,password} = req.body;    //withou this write req.body.name,req.body.email .. 
 
-    try{
+    const { name, email, password } = req.body;    //withou this write req.body.name,req.body.email .. 
+
+    try {
 
         // See if User Exits
-        let user=await User.findOne({email})    //email match karay ga agar user alredy ho ga
-        
-        if(user){
-            res.status(400).json({errors:[{msg:'User Already Exist'}]})
+        let user = await User.findOne({ email })    //email match karay ga agar user alredy ho ga
+
+        if (user) {
+            res.status(400).json({ errors: [{ msg: 'User Already Exist' }] })
         }
 
         // Get user Gravator
-        const avatar=gravatar.url(email,{
-            s:'200',   //size
-            r:'pg',   // radian
-            d:'mm'   // default   image if user not have gravatar
+        const avatar = gravatar.url(email, {
+            s: '200',   //size
+            r: 'pg',   // radian
+            d: 'mm'   // default   image if user not have gravatar
         })
 
-        user=new User({   //use to save user in db using user.save but first hash password
+        user = new User({   //use to save user in db using user.save but first hash password
             name,
             email,
             avatar,
@@ -50,22 +52,39 @@ router.post('/',[
 
         })
         // Encrypt Password
-        
-        const salt=await bcrypt.genSalt(10);
 
-        user.password=await bcrypt.hash(password,salt);
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
 
         await user.save();
 
         // Return Json webtoken
-        
+
+        const payload = {
+            user: {
+                id: user.id,       //its mean mongodb id which assign is user._id
+            }
+        };
+
+        jwt.sign(
+            payload,                 //payload
+            config.get('jwtSecret'),  //sectroet
+            { expiresIn: 360000 },      //optional
+            (err, token) => {           //call back which send error if error token if not error
+                if (err) throw err;
+                res.json({ token });
+
+            }
+        )
+
         res.send("User Register")
     }
-    catch(err){
+    catch (err) {
         console.error(err.message)
         res.status(500).send('Server Error')
     }
-    
+
 })
 
 module.exports = router;
