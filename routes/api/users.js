c3240ce9b0f8e642a34e6b6a190de491870bf4f5
Hammad <muhammadhammadali112233@gmail.com>
2022-04-17 17:37:08 +0500

    app.use(express.json({ extended:false})) in Server as a middle ware also we use Express validator in users.js and apply the validation in router.post , using [ ] array

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router=express.Router();
 
-// @route     GET api/users
-// @desc      Test route
+// @route     POST api/users
+// @desc      Register User
 // @access    Publie     for token 
 
-router.get('/',(req,res)=>res.send("User Routes"))
+router.post('/',[
+    check('name','Name is Required')
+    .not()
+    .isEmpty(),
+    check('email','Email is Required').isEmail(),
+    check('password','Please Enter a Password with 6 or More Characters').isLength({min:6}),
+
+],(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+
+    
+    console.log(req.body)
+    res.send("User Routes")
+
+})
 
 module.exports = router;
