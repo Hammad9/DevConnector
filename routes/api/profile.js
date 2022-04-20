93a2a74345cfe921be5551fe562e6a7cd91086ec
Hammad <muhammadhammadali112233@gmail.com>
2022-04-20 12:40:51 +0500

    Here we Get the User profile. Mean when we login through post man its Generate the token. When we copy this token into the  Profile x-auth-token we get the request that profile page is access but not have any data

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,32 @@
 const express = require('express');
-const router=express.Router();
+const router = express.Router();
+const auth = require('../../middleware/auth')
 
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 // @route     GET api/profile
 // @desc      Test route
 // @access    Publie     for token 
 
-router.get('/',(req,res)=>res.send("Profile Routes"))
+router.get('/me', auth, async (req, res) => {
+    try {
+
+        const profile = await Profile.findOne({ user: req.user.id }).populate(  
+            //user may say name and avatar ko get kiya then check what profile is or not
+            'user',
+            ['name', 'avatar']
+        );
+
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is No profile for this User' })
+        }
+        res.json(profile);
+         
+    }
+    catch (err) {
+        console.log(err.message)
+       return res.status(500).send('Server Error')
+    }
+})
 
 module.exports = router;
